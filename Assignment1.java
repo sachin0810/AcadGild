c58bb85c7931d05b2c709b7766c523d4119bb45a
sachin0810 <sachin0810@gmail.com>
2016-03-26 13:48:31 +0530

    Added files via upload

---
+++ Assignment1.java
@@ -0,0 +1,13 @@
+
+public class Assignment1 {
+	public static void main(String[] args) {
+		int a;
+		float b;
+		a=12;
+		b=(float) 45.12;
+		System.out.print("\nInteger Number : "+ a);
+		System.out.print("\nFloat Number : "+ b);
+		System.out.print("\nSum of Both :"+ (int)(a+b));
+		
+	}
+}
