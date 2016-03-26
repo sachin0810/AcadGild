19b5c209c9d8a6adc041bfb364781811499a7da0
sachin0810 <sachin0810@gmail.com>
2016-03-26 14:27:27 +0530

    Added files via upload

---
+++ Assign4.java
@@ -0,0 +1,17 @@
+
+public class Assign4 {
+
+	public static void main(String[] args) {
+	
+		int x=5,y=10;
+		System.out.println("Value of X: " + x);
+		System.out.println("Value of Y: " + y);
+		
+		System.out.println("Ans of X + Y*2 = "+ x+ y*2);
+		System.out.println("Ans of (X-Y)*2 = "+ (x-y)*2);
+		
+		System.out.println("Ans of (X + Y)*2 = "+ (x+y)*2);
+		System.out.println("Ans of  Y % X = "+ (float)(y%x));
+		
+}
+}
